d4c82550b87762c7c651398952f7c8f31ef461e3
Bran Liang <lby89757@hotmail.com>
2018-03-17 17:24:20 +0800

    Move the server start to a command

---
+++ cmd/server.go
@@ -0,0 +1,42 @@
+package cmd
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/cobra"
+)
+
+// serverCmd represents the server command
+var serverCmd = &cobra.Command{
+	Use:     "server",
+	Aliases: []string{"s"},
+	Short:   "Server start",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		r := gin.Default()
+		r.GET("/ping", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"message": "pong",
+			})
+		})
+		r.Run()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(serverCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// serverCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// serverCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
--- main.go
+++ main.go
@@ -2,18 +2,9 @@ package main
 
 import (
 	"github.com/branLiang/gin-playground/cmd"
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	// 初始化命令行工具集
 	cmd.Execute()
-
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run()
 }
