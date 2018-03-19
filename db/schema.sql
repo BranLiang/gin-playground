ba4d8d1a033f983494ee29ce1132825998eeabd4
Bran Liang <lby89757@hotmail.com>
2018-03-19 14:18:34 +0800

    Update the database column names

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,18 @@
+-- TABLES
 CREATE TABLE users (
   id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
   name varchar(255)
 );
 
 CREATE TABLE tasks (
-  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+  user_id uuid,
   title varchar(255),
   note text,
   complete boolean,
   created_at timestamp
 );
+
+-- SEEDS
+INSERT INTO users (name) VALUES ('Adam');
+INSERT INTO users (name) VALUES ('Bran');
